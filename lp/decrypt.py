279ee2e41b816485bb6718183ecc7fdb78a33b70
vyomchha <vyomchhabra@gmail.com>
2016-04-05 00:09:23 -0400

    lp

--- lp/decrypt.py
+++ lp/decrypt.py
@@ -6,9 +6,9 @@ def dec(s,c1,c2,n1,n2,q):
 			p[x] = int((p[x] + (s[x][y]*c1[n]))%(q/8))
 		n=n+1
 
-	print p
+	#print p
 	for x in range(n1):
 		p[x] = int((p[x] + c2[x])%q)
 
-	print p
+	#print p
 	return p
